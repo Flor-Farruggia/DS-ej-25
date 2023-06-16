e70c6cd9a4894d20e2d83f2d89c410c0256c8dc5
FlorFarruggia <flor.farru@gmail.com>
2023-06-16 19:26:00 -0300

    Arreglo final

--- index.php
+++ index.php
@@ -2,20 +2,44 @@
 
 require_once 'models/provincia.php';
 require_once 'models/direccion.php';
-require_once 'models/datosPersona.php';
+require_once 'models/datoPersona.php';
 require_once 'models/prestamo.php';
 require_once 'models/lineaPrestamo.php';
 
-/*
-$n = new Nation ();
-$n->id = 01;
-$n->description = 'This is a low description of this nation';
+$lp = new LineaPrestamo();
+$lp->Id = 1;
+$lp->Descripcion = 'Personal Sola Firma';
+$lp->TasaEfectivaMensual= '15 %';
 
-$p = new Person ();
-$p->name = 'Jhon Doe';
-$p->address = 'P.Sherman 42 Wallaby street';
-$p->dni = 11222333;
-$p->age = 33 ;
-$p->nationality = $n;
+$prv = new Provincia();
+$prv->Id = 1;
+$prv->Descripcion = 'Santa Fe';
 
-$p->showData();*/
+
+$dir = new Direccion();
+$dir->Id = 1;
+$dir->Calle = 'Montevideo';
+$dir->Numero = 34;
+$dir->Localidad = 'Rosario';
+$dir->Provincia = $prv;
+
+$dt = new DatosPersona(); 
+$dt->Id = 1;
+$dt->Nombre = 'Daniel';
+$dt->Apellido ='Rodriguez';
+$dt->Documento = 12456789;
+$dt->Direccion = $dir;
+
+$p = new Prestamo();
+$p->Id = 1;
+$p->LineaPrestamo = $lp;
+$p->Titular = $dt;
+$p->FechaAlta = '21/08/2019';
+$p->CantidadCuotas = 36;
+$p->MontoCapital = 10000;
+
+$p->MostrarDatos();
+
+
+
+
